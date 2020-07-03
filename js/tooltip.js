11015dbcc24aa268540b2b79c4781e85a83ab049
minhbvhp <tranminhbvhp@gmail.com>
2020-07-03 16:12:01 +0700

    update tooltips

---
+++ js/tooltip.js
@@ -0,0 +1,8 @@
+$(".more_info").click(function () {
+    var $title = $(this).find(".title");
+    if (!$title.length) {
+        $(this).append('<span class="title">' + $(this).attr("title") + '</span>');
+    } else {
+        $title.remove();
+    }
+});
